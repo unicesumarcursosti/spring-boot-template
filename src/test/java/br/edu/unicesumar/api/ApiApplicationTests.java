7e02e4613cac28aa8f73b987d4a3f85dd28bf5a5
Rafael Labegalini <rjlabegalini8@gmail.com>
2025-05-28 14:01:09 -0300

    Initial Commit

---
+++ src/test/java/br/edu/unicesumar/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
